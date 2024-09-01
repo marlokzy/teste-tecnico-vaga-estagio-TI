32bd7f2ec749566aa0ba735bfa62ac708b1f391e
marlokzy <marlon.r12325@gmail.com>
2024-08-31 22:28:01 -0300

    first commit

---
+++ Desafios/Desafio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// 1) Observe o trecho de código abaixo: 
+// int INDICE = 13, SOMA = 0, K = 0;
+// Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+// Imprimir(SOMA);
+// Ao final do processamento, qual será o valor da variável SOMA?
+
+namespace desafio_1;
+
+    public class DesafioUm
+    {
+        public static void Main(){
+            int INDICE = 13;
+            int SOMA = 0;
+            int K = 0;
+
+            while (K < INDICE)
+            {
+                // K = K + 1;
+                K++;
+                // SOMA = SOMA + K;
+                SOMA += K;
+            }
+
+            Console.WriteLine(SOMA);  // O valor da variável SOMA é 91!
+        }
+    }
---
+++ Desafios/Desafio 2/Program.cs
@@ -0,0 +1,35 @@
+﻿// 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+// IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
+
+namespace desafio_2;
+
+public class DesafioDois
+{
+    public static void Main()
+    {
+        // input
+        Console.WriteLine("Saiba a sequência de Fibonacci!!!");
+        Console.WriteLine("Digite um número:");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int auxiliar = number + number;
+
+        while (number <= auxiliar)
+        {
+            if (auxiliar >= number)
+            {
+                Console.WriteLine($"O número {number} pertence a sequência Fibonacci");
+                number += auxiliar;
+            }
+            else
+            {
+                Console.WriteLine($"O número {number} não pertence a sequência Fibonacci");
+                break;
+            }
+            number += auxiliar;
+        }
+    }
+}
+
+// EM CONSTRUÇÃO - TENTANDO RESOLVER
